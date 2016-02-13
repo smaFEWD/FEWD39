fd17624edf0c3925b79ce358a64b626548a93cac
Sandi <sandima.acro@gmail.com>
2016-02-12 23:20:44 -0800

    included week 6, 7, 8 labs and solutions

---
+++ code_along_solutions/week7/starter_code_lesson_07_sol/traffic_light/js/index.js
@@ -0,0 +1,4 @@
+//Implement the red light using jQuery. Don't forget to add the script tags in your html.
+$("#stopButton").on("click", function() {
+  $('#stopLight').css('background-color', 'red');
+});
